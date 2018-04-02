b25ad83a7e5b6311c2fa2040ece98c64567f0ac7
Rasa Cosmin <rasa.cosmin@gmail.com>
2018-04-02 17:35:24 +0300

    add missing files to prev commit

--- app/src/main/java/com/rasik/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/rasik/coderswag/Adapters/CategoryAdapter.kt
@@ -1,7 +1,45 @@
 package com.rasik.coderswag.Adapters
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.rasik.coderswag.Model.Category
+import com.rasik.coderswag.R
+
 /**
  * Created by Rasa Cosmin on 02.04.2018.
  */
-class CategoryAdapter {
+class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter() {
+    val context = context
+    val categories = categories
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val categoryView: View
+        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+        val categoryImage: ImageView = categoryView.findViewById(R.id.categoryImage)
+        val categoryName: TextView = categoryView.findViewById(R.id.categoryName)
+        val category = categories[position]
+
+        categoryName.text = category.title
+        val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+        categoryImage.setImageResource(resourceId)
+
+        return  categoryView
+    }
+
+    override fun getItem(position: Int): Any {
+       return categories[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+       return categories.count()
+    }
 }
