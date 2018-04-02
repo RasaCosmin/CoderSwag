e5cbba1a15dd3738cd6a14fb65d479995a5182f4
Rasa Cosmin <rasa.cosmin@gmail.com>
2018-04-02 15:24:20 +0300

    with prev

--- app/src/main/java/com/rasik/coderswag/Model/Category.kt
+++ app/src/main/java/com/rasik/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.rasik.coderswag.Model
 /**
  * Created by Rasa Cosmin on 02.03.2018.
  */
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+}
